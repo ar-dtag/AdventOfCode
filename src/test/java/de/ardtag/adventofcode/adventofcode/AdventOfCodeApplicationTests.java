13844da7920ea3602ba696d9adde16ac2ca2ec66
André Riehl <andre.riehl@telekom.de>
2022-12-01 09:44:03 +0100

    Initial commit

---
+++ src/test/java/de/ardtag/adventofcode/adventofcode/AdventOfCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.ardtag.adventofcode.adventofcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventOfCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
