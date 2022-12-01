8ce1073ba68a7e44844046cbdfa75c0b5c0e8bd6
André Riehl <andre.riehl@telekom.de>
2022-12-01 15:44:21 +0100

    Day1 initial commuit

--- src/main/java/de/ardtag/adventofcode/adventofcode/Day1/Day1.java
+++ src/main/java/de/ardtag/adventofcode/adventofcode/Day1/Day1.java
@@ -3,14 +3,53 @@ package de.ardtag.adventofcode.adventofcode.Day1;
 import de.ardtag.adventofcode.adventofcode.Helpers.FileImporter;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day1 {
     public void run() throws IOException {
         FileImporter myFileImporter = new FileImporter();
+        List<Integer> ElveList = new ArrayList<>();
+        AtomicInteger intCalories = new AtomicInteger();
 
         myFileImporter.readFile("Day1.txt").forEach(s -> {
-            System.out.println(s);
+
+            if (!s.isEmpty()) {
+                intCalories.getAndAdd(Integer.parseInt(s));
+            }
+            else {
+                ElveList.add(intCalories.get());
+                intCalories.set(0);
+            }
         });
+
+        ElveList.forEach(integer -> {
+          System.out.println(integer);
+        });
+
+
+        // Create maxValue variable and initialize with 0
+        int maxValue = 0;
+
+        // Check maximum element using for loop
+        for (Integer integer : ElveList) {
+            if (integer > maxValue)
+                maxValue = integer;
+        }
+        System.out.println("The maximum value is "
+                + maxValue);
+
+        System.out.println("The sorted List is :");
+
+        //sort reverse
+        ElveList.sort((o1, o2) -> o2.compareTo(o1));
+
+        ElveList.forEach(integer -> {
+            System.out.println(integer);
+        });
+
+
+
     }
 }
